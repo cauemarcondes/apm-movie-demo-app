997542d33b29779c4123ea7c9451e2ed27ff5264
cauemarcondes <caue.marcondes@elastic.co>
2021-11-16 12:45:21 -0500

    adding routes

--- movie-frontend/src/App.tsx
+++ movie-frontend/src/App.tsx
@@ -1,11 +1,29 @@
+//@ts-ignore
+import { ApmRoute } from "@elastic/apm-rum-react";
+import { BrowserRouter as Router, Link, Switch } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
 import MovieList from "./components/MovieList";
 
 function App() {
   return (
-    <div>
-      <MovieList />
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/movies">Movies</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <ApmRoute exact path="/" component={Home}></ApmRoute>
+          <ApmRoute path="/movies" component={MovieList}></ApmRoute>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
